23d8d18a2b15eafc45a5a0bd5483908211ba696a
Izet Molla <imolla@uet.com>
2021-10-15 15:39:16 +0200

    new

---
+++ index.d.ts
@@ -0,0 +1,35 @@
+// Type definitions for react-chat-theme v3.0.0
+// Project: <https://github.com/izetmolla/react-chat-theme>
+// Definitions by: Izet Molla <https://github.com/izetmolla>
+
+import { ElementType } from 'react';
+
+declare const Widget: ElementType;
+
+export function addUserMessage(text: string): void;
+export function addUserMessage(text: string, id: string): void;
+
+export function addResponseMessage(text: string): void;
+export function addResponseMessage(text: string, id: string): void;
+
+export function addLinkSnippet(link: { link: string, title: string, target?: string }): void;
+export function addLinkSnippet(link: { link: string, title: string, target?: string }, id: string): void;
+
+export function renderCustomComponent(component: ElementType, props: any): void;
+export function renderCustomComponent(component: ElementType, props: any, showAvatar: boolean): void;
+export function renderCustomComponent(component: ElementType, props: any, showAvatar: boolean, id: string): void;
+
+export function toggleMsgLoader(): void;
+export function toggleWidget(): void;
+export function toggleInputDisabled(): void;
+export function dropMessages(): void;
+export function isWidgetOpened(): boolean;
+export function setQuickButtons(buttons: Array<{ label: string, value: string | number }>): void;
+
+export function deleteMessages(count: number): void;
+export function deleteMessages(count: number, id: string): void;
+
+export function markAllAsRead(): void;
+export function setBadgeCount(count: number): void;
+
+export as namespace ReactChatWidget;
